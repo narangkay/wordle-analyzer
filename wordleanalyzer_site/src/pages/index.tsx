0e2d7524e5ee087ed1871ddf9546b0087369bd9b
insurgentes <kanarang@gmail.com>
2022-01-14 01:59:42 +0530

    add trpc

--- wordleanalyzer_site/src/pages/index.tsx
+++ wordleanalyzer_site/src/pages/index.tsx
@@ -1,4 +1,9 @@
+import { trpc } from "@/utils/trpc"
+
 const Home = () => {
+  const { data, isLoading } = trpc.useQuery(["hello", { text: "Theo" }]);
+  if (isLoading) return <div>Loading...</div>
+  if (data) return <div>{data.greeting}</div>
   return (<div className="w-screen flex flex-col h-screen justify-center items-center">
     <div className="text-2xl text-center">How good is your starting word?</div>
     <div className='p-2'></div>
