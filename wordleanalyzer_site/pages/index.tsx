3cfbd0b5c24806ba81a37eab4eabea5fa3e7dc32
insurgentes <kanarang@gmail.com>
2022-01-13 21:50:11 +0530

    add site for wordle analyzing

---
+++ wordleanalyzer_site/pages/index.tsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (<div className="w-screen flex flex-col h-screen justify-center items-center">
+    <div className="text-2xl text-center">How good is your starting word?</div>
+    <div className='p-2'></div>
+    <div className="border rounded p-8 flex flex-col justify-between max-w-2xl items-center">
+      <div className='w-80 h-16 text-center fkex'>Top 10 starting words</div>
+      <div className='w-80 h-16 text-center fkex'></div>
+    </div >
+  </div >
+  )
+}
+
+export default Home
