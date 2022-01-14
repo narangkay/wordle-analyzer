394a981fd7ac5a5c4535905cc7f25d6e24d69259
insurgentes <kanarang@gmail.com>
2022-01-15 04:45:36 +0530

    temporarily comment out rank by success rate needed because of weird vercel behaviour

--- wordleanalyzer_site/src/pages/InitialWordCard.tsx
+++ wordleanalyzer_site/src/pages/InitialWordCard.tsx
@@ -32,9 +32,9 @@ const InitialWordCard: React.FC<{
             {/* <div className="py-4 px-5">
                 {result.rankByGuessesNeeded}
             </div> */}
-            <div className="py-4 px-5">
+            {/* <div className="py-4 px-5">
                 {result.rankBySuccessRate}
-            </div>
+            </div> */}
         </div>);
 };
 
