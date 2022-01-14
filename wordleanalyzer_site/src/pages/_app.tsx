a4496e28ca0c7112050dc197af5c5c58ee526c3f
insurgentes <kanarang@gmail.com>
2022-01-14 22:18:07 +0530

    trying to debug vercel url issue

--- wordleanalyzer_site/src/pages/_app.tsx
+++ wordleanalyzer_site/src/pages/_app.tsx
@@ -17,7 +17,7 @@ export default withTRPC<AppRouter>({
     const url = process.env.VERCEL_URL
       ? `https://${process.env.VERCEL_URL}/api/trpc`
       : 'http://localhost:3000/api/trpc';
-
+    console.log(url);
     return {
       url,
       /**
