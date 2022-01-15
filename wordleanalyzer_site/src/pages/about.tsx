813b0de98113ce1f2071ab217b59ebd6cd4b6c41
insurgentes <kanarang@gmail.com>
2022-01-15 08:00:08 +0530

    fix some escaping

--- wordleanalyzer_site/src/pages/about.tsx
+++ wordleanalyzer_site/src/pages/about.tsx
@@ -6,15 +6,15 @@ const AboutPage = () => {
             <h2 className="text-4xl">About</h2>
             <div className="p-2"></div>
             <p className="max-w-xl">
-                I made this because I'm obsessed with wordle and I think you should be obsessed with wordle too.
+                I made this because I&apos;m obsessed with wordle and I think you should be obsessed with wordle too.
             </p>
             <div className="p-4"></div>
             <h1 className="text-4xl">Methodology</h1>
             <div className="p-2"></div>
             <p className="max-w-xl">
                 Wordle Analyzer pits every 5-letter word in existence as the starting word against every other 5-letter word in existence as the hidden word, and tries to figure out which words are best suited as starting words.
-                <br /><br />Subsequent words (after the starting word) are selected by an algorithm that picks any valid word that does not violate the hints we get from the words we've already played.
-                <br /><br /> The algorithm is non-trivial, but as an example, if we have already played "table" and "weird" for the hidden word "peels", we won't play any more words with the letters "tablwird", and we will only play words with e in the second position.
+                <br /><br />Subsequent words (after the starting word) are selected by an algorithm that picks any valid word that does not violate the hints we get from the words we&apos;ve already played.
+                <br /><br /> The algorithm is non-trivial, but as an example, if we have already played &quot;table&quot; and &quot;weird&quot; for the hidden word &quot;peels&quot;, we won&apos;t play any more words with the letters &quot;tablwird&quot;, and we will only play words with e in the second position.
                 <br /><br /> Details of the algorithm can be found in the code, open-sourced on github.
                 <br /><br />
             </p>
