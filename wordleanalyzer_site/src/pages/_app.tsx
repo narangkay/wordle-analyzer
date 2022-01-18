daf83aaaf1df21084403a61e7d962bcc1f380eb2
insurgentes <kanarang@gmail.com>
2022-01-19 03:06:07 +0530

    fix qps

--- wordleanalyzer_site/src/pages/_app.tsx
+++ wordleanalyzer_site/src/pages/_app.tsx
@@ -1,11 +1,33 @@
 import { withTRPC } from '@trpc/next';
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 import 'tailwindcss/tailwind.css'
 import '../styles/global.css'
 import { AppRouter } from './api/trpc/[trpc]';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+          page_path: window.location.pathname,
+        });
+      `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>)
 }
 
 
